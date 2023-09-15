9936e266ee06e9108f7dc724005713a360e3844a
Tanner Shimanek <tannershimanek@gmail.com>
2023-09-14 19:53:47 -0600

    added config

---
+++ config.py
@@ -0,0 +1,10 @@
+BANK_CONFIG = {
+  'intital_user_id': 0,
+  'loan_key': 1,
+  'savings_key': 2,
+  'interest_rate': 0.12,
+  'savings_rate': 0.03,
+  'min_loan_amount': 500,
+  'max_loan_amount': 50_000,
+  'late_fee': 50,
+}
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import config, copy
+CONFIG = config.BANK_CONFIG
+
+
 def main():
+
     print("Hello World!")
+    print(copy.copy(CONFIG.get('interest_rate')))
 
 
 if __name__ == '__main__':
