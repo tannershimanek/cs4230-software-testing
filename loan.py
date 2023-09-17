45965665412ff940ca2ab34fb3ec0f73537bd9b5
Walter Smith <WalterS@natr.com>
2023-09-16 20:19:42 -0600

    begin Loan class

---
+++ loan.py
@@ -0,0 +1,13 @@
+import config
+CONFIG = config.BANK_CONFIG
+
+class Loan:
+    def __init__(self, id, balance) -> None:
+        self.id = id + CONFIG.get('loan_key') #Will also need the current loan number 
+        self.interest_rate = CONFIG.get('interest_rate')
+        self.balance = balance
+        self.interest_added = 0
+        self.late_fee = True
+
+    def __str__(self):
+        return f"Loan (ID = {self.id}, Balance = {self.balance}, IntRate = {self.interest_rate:.0%})"
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import config, copy
+from loan import Loan
 CONFIG = config.BANK_CONFIG
-
 
 def main():
 
     print("Hello World!")
     print(copy.copy(CONFIG.get('interest_rate')))
+
+    loan = Loan(101, 1000)
+    print(loan)
 
 
 if __name__ == '__main__':
